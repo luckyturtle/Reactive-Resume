0cf47bdfc400ac5f16384fb5534ad6eba81c25fc
Ninja <evangelou.e.95@gmail.com>
2022-03-02 21:24:09 +0200

    share url

--- src/components/builder/right/sections/Actions.js
+++ src/components/builder/right/sections/Actions.js
@@ -28,7 +28,7 @@ const Actions = ({ id }) => {
 
   const getSharableUrl = () => {
     const shareId = state.id;
-    return `https://rxresu.me/r/${shareId}`;
+    return `https://reactiveresumemaster.gatsbyjs.io/r/${shareId}`;
   };
 
   const handleOpenLink = () => {
